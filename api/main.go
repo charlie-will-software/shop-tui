da6026ade34ff955d0a00dcedf33cbbd63aff196
Will Dalgleish <dev@wdalgleish.com>
2024-08-18 14:07:29 +0100

    feat: get items endpoint
    adds a GET handler for /items

--- api/main.go
+++ api/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"charlie-will-software/shop-tui/api/model"
 )
+
+var items = []model.Item{
+	{Id: 1, Title: "Bread", Price: 1.09},
+	{Id: 2, Title: "Milk", Price: 1.5},
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/", index)
+	router.GET("/items", getItems)
 
 	router.Run("0.0.0.0:8080")
 }
@@ -17,4 +24,8 @@ func index(c *gin.Context) {
 	jsonData := []byte(`{"msg": "Hello world!"}`)
 
 	c.Data(http.StatusOK, "application/json", jsonData)
+}
+
+func getItems(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, items)
 }
